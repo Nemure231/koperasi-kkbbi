21503b6f7f0cba3014bdd0efee4a15e1d6d7b5fd
Nemure231 <neruhi5321@gmail.com>
2021-03-01 14:51:47 +0700

    menghapus asArray di query first  pada controller satuan

--- app/Controllers/Satuan.php
+++ app/Controllers/Satuan.php
@@ -38,7 +38,7 @@ class Satuan extends BaseController
 
         $data = [
             'title' => ucfirst('Daftar Satuan'),
-            'user' 	=> 	$this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')->asArray()
+            'user' 	=> 	$this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')
 						->join('user_role', 'user_role.id_role = user.role_id')
 						->where('email', $email)
 						->first(),
