3f8cf3c75468247686f18863a92767c894bdf1a4
Nemure231 <neruhi5321@gmail.com>
2021-03-01 17:42:18 +0700

    Menghubungkan contoller menu dan submenu dengan model dan memperbaiki route

---
+++ app/Models/Model_user_sub_menu.php
@@ -0,0 +1,13 @@
+<?php namespace App\Models;
+use CodeIgniter\Model;
+ 
+class Model_user_sub_menu extends Model{
+
+    protected $table = 'user_sub_menu';
+    protected $primaryKey = 'id_submenu';
+    protected $returnType = 'array';
+    protected $allowedFields = ['menu_id', 'judul', 'url', 'icon', 'is_active'];
+
+    
+}
+?>
