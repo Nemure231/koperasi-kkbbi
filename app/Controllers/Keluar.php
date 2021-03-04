14d1cfc9ef0043a57bf2afcfb1f413a8a824aec2
Nemure231 <neruhi5321@gmail.com>
2021-03-04 16:57:12 +0700

    Menghubungkan controller barang keluar dan masuk dengan model

--- app/Controllers/Keluar.php
+++ app/Controllers/Keluar.php
@@ -2,12 +2,20 @@
 
 use CodeIgniter\Controller;
 use App\Models\Model_all;
+use App\Models\Model_user;
+use App\Models\Model_toko;
+use App\Models\Model_user_menu;
+use App\Models\Model_transaksi_total;
 
 class Keluar extends BaseController{
 
     public function __construct(){
 
 		$this->model = new Model_all();
+        $this->model_user_menu = new Model_user_menu();
+		$this->model_user = new Model_user();
+        $this->model_toko = new Model_toko();
+        $this->model_transaksi_total = new Model_transaksi_total();
 		$this->request = \Config\Services::request();
 	}
 
@@ -17,6 +25,7 @@ class Keluar extends BaseController{
     public function index(){
 		
 		$role = $this->session->get('role_id');
+        $email = $this->session->get('email');
 		
 		if (!$role){
             return redirect()->to(base_url('/'));
@@ -28,12 +37,12 @@ class Keluar extends BaseController{
 
         $tanggal = $this->request->getPost('cari_tanggal');
 
-        $hari = $this->model->GetAllBarangKeluarHariCari();
+        $hari = $this->model_transaksi_total->GetAllBarangKeluarHariCari();
         $tanggal_ini = date('Y-m-d');
         $pesan =  '';
 
         if($tanggal){
-            $hari = $this->model->GetAllBarangKeluarHariCari($tanggal);
+            $hari = $this->model_transaksi_total->GetAllBarangKeluarHariCari($tanggal);
             $tanggal_ini = $tanggal;
             $pesan = '<div class="alert alert-success alert-dismissible show fade">
             <div class="alert-body">
@@ -59,17 +68,27 @@ class Keluar extends BaseController{
 
         if($gaya == 3){
             $body = 'admin/admin-laporan-keluar-hari/v_laporan_keluar_hari_list_supplier';
-            $hari = $this->model->GetAllBarangKeluarHariCariTambah($tanggal);
+            $hari = $this->model_transaksi_total->GetAllBarangKeluarHariCariTambah($tanggal);
         }
     
     
         $data = [
            
-           'title' => ucfirst('Laporan Barang Keluar Harian'),
-           'user' => $this->model->UserLogin(),
-           'menu' => $this->model->MenuAll(),
+            'title'  => ucfirst('Laporan Barang Keluar Harian'),
+            'user' 	=>  $this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')->asArray()
+                    ->join('user_role', 'user_role.id_role = user.role_id')
+                    ->where('email', $email)
+                    ->first(),
+            'menu' 	=> 	$this->model_user_menu->select('id_menu, menu')->asArray()
+                    ->join('user_access_menu', 'user_access_menu.menu_id = user_menu.id_menu')
+                    ->where('user_access_menu.role_id =', $role)
+                    ->orderBy('user_access_menu.menu_id', 'ASC')
+                    ->orderBy('user_access_menu.role_id', 'ASC')
+                    ->findAll(),
            'session' => $this->session,
-           'toko' => $this->model->GetRowTokoForLaporan(),
+           'toko' => $this->model_toko->select('nama_toko, telepon_toko, alamat_toko, logo_toko, logo_koperasi_inter')
+                    ->asArray()->where('id_toko', 1)
+                    ->first(),
            'hari' => $hari,
            'pesan_hari' => $pesan,
            'tanggal_ini' => $tanggal_ini,
@@ -84,15 +103,13 @@ class Keluar extends BaseController{
            ],
            
         ];
-
-    
-        //dd();
         tampilan_admin($body, 'admin/admin-laporan-keluar-hari/v_js_laporan_keluar_hari', $data);
     }
 
     public function barangkeluarminggu(){
 		
 		$role = $this->session->get('role_id');
+        $email = $this->session->get('email');
 		
 		if (!$role){
             return redirect()->to(base_url('/'));
@@ -113,7 +130,7 @@ class Keluar extends BaseController{
         $pesan =  '';
 
         if($awal_minggu && $akhir_minggu){
-            $minggu = $this->model->GetAllBarangKeluarMingguCari($awal_minggu, $akhir_minggu);
+            $minggu = $this->model_transaksi_total->GetAllBarangKeluarMingguCari($awal_minggu, $akhir_minggu);
             $minggu_ini = $awal_minggu.' ~ '.$akhir_minggu;
             $pesan = '<div class="alert alert-success alert-dismissible show fade">
             <div class="alert-body">
@@ -140,16 +157,26 @@ class Keluar extends BaseController{
          
         if($gaya == 3){
             $body = 'admin/admin-laporan-keluar-minggu/v_laporan_keluar_minggu_list_supplier';
-            $minggu = $this->model->GetAllBarangKeluarMingguCariTambah($awal_minggu, $akhir_minggu);
+            $minggu = $this->model_transaksi_total->GetAllBarangKeluarMingguCariTambah($awal_minggu, $akhir_minggu);
         }
         
         $data = [
            
-           'title' => ucfirst('Laporan Barang Keluar Mingguan'),
-           'user' => $this->model->UserLogin(),
-           'menu' => $this->model->MenuAll(),
-           'session' => $this->session,
-           'toko' => $this->model->GetRowTokoForLaporan(),
+            'title' => ucfirst('Laporan Barang Keluar Mingguan'),
+            'user' 	=>  $this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')->asArray()
+                    ->join('user_role', 'user_role.id_role = user.role_id')
+                    ->where('email', $email)
+                    ->first(),
+            'menu' 	=> $this->model_user_menu->select('id_menu, menu')->asArray()
+                    ->join('user_access_menu', 'user_access_menu.menu_id = user_menu.id_menu')
+                    ->where('user_access_menu.role_id =', $role)
+                    ->orderBy('user_access_menu.menu_id', 'ASC')
+                    ->orderBy('user_access_menu.role_id', 'ASC')
+                    ->findAll(),
+            'session'=> $this->session,
+            'toko'  =>$this->model_toko->select('nama_toko, telepon_toko, alamat_toko, logo_toko, logo_koperasi_inter')
+                    ->asArray()->where('id_toko', 1)
+                    ->first(),
            'minggu' => $minggu,
            'pesan_minggu' => $pesan,
            'form_minggu' =>  ['id' => 'formMinggu', 'name'=>'formMinggu'],
@@ -170,11 +197,7 @@ class Keluar extends BaseController{
             'class' => 'form-control',
             'required' => ''
         ]
-           
         ];
-
-    
-        //dd();
         tampilan_admin($body, 'admin/admin-laporan-keluar-minggu/v_js_laporan_keluar_minggu', $data);
     }
 
@@ -182,6 +205,7 @@ class Keluar extends BaseController{
     public function barangkeluarbulan(){
 		
 		$role = $this->session->get('role_id');
+        $email = $this->session->get('email');
 		
 		if (!$role){
             return redirect()->to(base_url('/'));
@@ -196,11 +220,11 @@ class Keluar extends BaseController{
         $month = $this->request->getPost('cari_bulan');
         $year = $this->request->getPost('cari_tahun');
 
-        $bulan = $this->model->GetAllBarangKeluarBulanCari();
+        $bulan = $this->model_transaksi_total->GetAllBarangKeluarBulanCari();
         $tanggal_ini = date('Y-m');
         $pesan =  '';
         if($month && $year){
-            $bulan = $this->model->GetAllBarangKeluarBulanCari($month, $year);
+            $bulan = $this->model_transaksi_total->GetAllBarangKeluarBulanCari($month, $year);
             $tanggal_ini = $year.'-'.$month;
             $pesan = '<div class="alert alert-success alert-dismissible show fade">
             <div class="alert-body">
@@ -227,16 +251,25 @@ class Keluar extends BaseController{
 
         if($gaya == 3){
             $body = 'admin/admin-laporan-keluar-bulan/v_laporan_keluar_bulan_list_supplier';
-            $bulan = $this->model->GetAllBarangKeluarBulanCariTambah($month, $year);
+            $bulan = $this->model_transaksi_total->GetAllBarangKeluarBulanCariTambah($month, $year);
         }
         
         $data = [
-           
-           'title' => ucfirst('Laporan Barang Keluar Bulanan'),
-           'user' => $this->model->UserLogin(),
-           'menu' => $this->model->MenuAll(),
-           'session' => $this->session,
-           'toko' => $this->model->GetRowTokoForLaporan(),
+            'title' => ucfirst('Laporan Barang Keluar Bulanan'),
+            'user' 	=>  $this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')->asArray()
+                    ->join('user_role', 'user_role.id_role = user.role_id')
+                    ->where('email', $email)
+                    ->first(),
+            'menu' 	=> 	$this->model_user_menu->select('id_menu, menu')->asArray()
+                    ->join('user_access_menu', 'user_access_menu.menu_id = user_menu.id_menu')
+                    ->where('user_access_menu.role_id =', $role)
+                    ->orderBy('user_access_menu.menu_id', 'ASC')
+                    ->orderBy('user_access_menu.role_id', 'ASC')
+                    ->findAll(),
+            'session'=> $this->session,
+            'toko'  =>$this->model_toko->select('nama_toko, telepon_toko, alamat_toko, logo_toko, logo_koperasi_inter')
+                    ->asArray()->where('id_toko', 1)
+                    ->first(),
            'bulan' => $bulan,
            'pesan_bulan' => $pesan,
            'form_bulan' =>  ['id' => 'formBulan', 'name'=>'formBulan'],
@@ -257,9 +290,6 @@ class Keluar extends BaseController{
             'class' => 'form-control',
             'required' => ''
         ]
-           
-           
-           
         ];
         tampilan_admin($body, 'admin/admin-laporan-keluar-bulan/v_js_laporan_keluar_bulan', $data);
     }
@@ -267,6 +297,7 @@ class Keluar extends BaseController{
     public function barangkeluartahun(){
 		
 		$role = $this->session->get('role_id');
+        $email = $this->session->get('email');
 		
 		if (!$role){
             return redirect()->to(base_url('/'));
@@ -280,12 +311,12 @@ class Keluar extends BaseController{
 
         $cari_tahun = $this->request->getPost('cari_tahun');
 
-        $tahun = $this->model->GetAllBarangKeluarTahunCari();
+        $tahun = $this->model_transaksi_total->GetAllBarangKeluarTahunCari();
         $tahun_ini = date('Y');
         $pesan =  '';
 
         if($cari_tahun){
-            $tahun = $this->model->GetAllBarangKeluarTahunCari($cari_tahun);
+            $tahun = $this->model_transaksi_total->GetAllBarangKeluarTahunCari($cari_tahun);
             $tahun_ini = $cari_tahun;
             $pesan = '<div class="alert alert-success alert-dismissible show fade">
             <div class="alert-body">
@@ -311,32 +342,41 @@ class Keluar extends BaseController{
 
         if($gaya == 3){
             $body = 'admin/admin-laporan-keluar-tahun/v_laporan_keluar_tahun_list_supplier';
-            $tahun = $this->model->GetAllBarangKeluarTahunCariTambah($cari_tahun);
+            $tahun = $this->model_transaksi_total->GetAllBarangKeluarTahunCariTambah($cari_tahun);
         }
         
     
         $data = [
-           
-           'title' => ucfirst('Laporan Barang Keluar Tahunan'),
-           'user' => $this->model->UserLogin(),
-           'menu' => $this->model->MenuAll(),
-           'session' => $this->session,
-           'test' => $this->model->Trop(),
-           'toko' => $this->model->GetRowTokoForLaporan(),
-           'tahun' => $tahun,
-           'tahun_masuk' => $this->model->GetAllBarangMasukTahunCari(),
-           'pesan_tahun' => $pesan,
-           'tahun_ini' => $tahun_ini,
-           'form_tahun' =>  ['id' => 'formTahun', 'name'=>'formTahun'],
-           'input_tahun' => [
-            'type' => 'text',
-            'name' => 'cari_tahun',
-            'id' => 'cari_tahun',
-            'placeholder' => 'Cari tahun ....',
-            'class' => 'form-control',
-            'required' => ''
-           ],
-           
+            'title' => ucfirst('Laporan Barang Keluar Tahunan'),
+            'user' 	=>  $this->model_user->select('id_user, nama, email, telepon, gambar, alamat, role')->asArray()
+                    ->join('user_role', 'user_role.id_role = user.role_id')
+                    ->where('email', $email)
+                    ->first(),
+            'menu' 	=> 	$this->model_user_menu->select('id_menu, menu')->asArray()
+                    ->join('user_access_menu', 'user_access_menu.menu_id = user_menu.id_menu')
+                    ->where('user_access_menu.role_id =', $role)
+                    ->orderBy('user_access_menu.menu_id', 'ASC')
+                    ->orderBy('user_access_menu.role_id', 'ASC')
+                    ->findAll(),
+            'session'=> $this->session,
+            'toko'  =>$this->model_toko->select('nama_toko, telepon_toko, alamat_toko, logo_toko, logo_koperasi_inter')
+                    ->asArray()->where('id_toko', 1)
+                    ->first(),
+            // 'test' => $this->model->Trop(),
+            'tahun' => $tahun,
+            // 'tahun_masuk' => $this->model->GetAllBarangMasukTahunCari(),
+            'pesan_tahun' => $pesan,
+            'tahun_ini' => $tahun_ini,
+            'form_tahun' =>  ['id' => 'formTahun', 'name'=>'formTahun'],
+            'input_tahun' => [
+                'type' => 'text',
+                'name' => 'cari_tahun',
+                'id' => 'cari_tahun',
+                'placeholder' => 'Cari tahun ....',
+                'class' => 'form-control',
+                'required' => ''
+            ],
+            
         ];
 
         tampilan_admin($body, 'admin/admin-laporan-keluar-tahun/v_js_laporan_keluar_tahun', $data);
